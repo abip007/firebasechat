27f6a48b4bd2c5d2e3afc0301f49cda9fb255dea
abip007 <abip007@gmail.com>
2018-04-05 13:48:23 +0600

    First commit

---
+++ src/providers/auth-provider/auth-provider.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class AuthProvider {
+  constructor(public af:AngularFire, public local:Storage) {}
+  
+  signin(credentails) {   
+    //console.log(this.af.auth.login(credentails));
+    return this.af.auth.login(credentails);
+    
+  }
+  
+  createAccount(credentails) {
+    return this.af.auth.createUser(credentails);
+  };
+  
+  logout() {
+     this.af.auth.logout();
+  }
+}
+
